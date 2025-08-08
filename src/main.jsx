68d5a3a7649f784b745710dce3830e9eedf5eaff
icandia-nvy <icandia@dslosangeles.cl>
2025-08-08 17:28:33 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles.css'
-import App from './App.jsx'
-
-const rootElement = document.getElementById('root')
-const root = createRoot(rootElement)
-root.render(<App />)
